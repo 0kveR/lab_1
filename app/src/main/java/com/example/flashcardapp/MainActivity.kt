4d0e3be3febd642f889eeab19d522502be98d688
Krrish Verma <krrish583@gmail.com>
2022-10-02 14:41:55 -0400

    Added editing function

--- app/src/main/java/com/example/flashcardapp/MainActivity.kt
+++ app/src/main/java/com/example/flashcardapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.TextView
@@ -18,8 +19,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var ans3: TextView
     private lateinit var questionSide: TextView
     private lateinit var answerSide: TextView
-    private lateinit var addBtn: ImageView
+    private lateinit var addFact: ImageView
     private lateinit var bg: RelativeLayout
+    private lateinit var editFact: ImageView
 
     @SuppressLint("SetTextI18n", "UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,23 +37,39 @@ class MainActivity : AppCompatActivity() {
             ans3 = mainBinding.ans3
             questionSide = mainBinding.questionSide
             answerSide = mainBinding.answerSide
-            addBtn = mainBinding.addBtn
+            addFact = mainBinding.addBtn
             bg = mainBinding.background
+            editFact = mainBinding.editBtn
         }
 
         val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             val data: Intent? = result.data
 
-            // TODO: Extract Data
             if (data != null) {
                 val questionString = data.getStringExtra("Question")
                 val answerString = data.getStringExtra("Answer")
+                val multi = data.getStringExtra("Multi")
 
                 questionSide.text = questionString
                 answerSide.text = answerString
-                ans1.visibility = View.INVISIBLE
-                ans2.visibility = View.INVISIBLE
-                ans3.visibility = View.INVISIBLE
+
+                when (multi) {
+                    "0" -> {
+                        ans1.visibility = View.INVISIBLE
+                        ans2.visibility = View.INVISIBLE
+                        ans3.visibility = View.INVISIBLE
+                    }
+                    "1" -> {
+                        ans1.text = data.getStringExtra("IncorrectAnswer1")
+                        ans2.visibility = View.INVISIBLE
+                        ans3.text = data.getStringExtra("Answer")
+                    }
+                    "3" -> {
+                        ans1.text = data.getStringExtra("IncorrectAnswer1")
+                        ans2.text = data.getStringExtra("IncorrectAnswer2")
+                        ans3.text = data.getStringExtra("Answer")
+                    }
+                }
             }
         }
 
@@ -71,8 +89,19 @@ class MainActivity : AppCompatActivity() {
 
         answerSide.setOnClickListener { conceal() }
 
-        addBtn.setOnClickListener {
+        addFact.setOnClickListener {
             val intent = Intent(this, NewFact::class.java)
+            intent.putExtra("EditMode", "false")
+            resultLauncher.launch(intent)
+        }
+
+        editFact.setOnClickListener {
+            val intent = Intent(this, NewFact::class.java)
+            intent.putExtra("EditMode", "true")
+            intent.putExtra("Question", questionSide.text.toString())
+            intent.putExtra("CorrectAnswer", answerSide.text.toString())
+            intent.putExtra("IncorrectAnswer", ans1.text.toString())
+            intent.putExtra("IncorrectAnswer2", ans2.text.toString())
             resultLauncher.launch(intent)
         }
 
--- app/src/main/java/com/example/flashcardapp/NewFact.kt
+++ app/src/main/java/com/example/flashcardapp/NewFact.kt
@@ -3,16 +3,18 @@ package com.example.flashcardapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.ImageView
-import android.widget.TextView
 import com.example.flashcardapp.databinding.ActivityNewFactBinding
 
 class NewFact : AppCompatActivity() {
     private lateinit var newFactBinding: ActivityNewFactBinding
     private lateinit var cancel: ImageView
     private lateinit var save: ImageView
-    private lateinit var question: TextView
-    private lateinit var answer: TextView
+    private lateinit var question: EditText
+    private lateinit var answer: EditText
+    private lateinit var incorrectAnswer1: EditText
+    private lateinit var incorrectAnswer2: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +26,17 @@ class NewFact : AppCompatActivity() {
             save = newFactBinding.saveBtn
             question = newFactBinding.addQuestion
             answer = newFactBinding.addAnswer
+            incorrectAnswer1 = newFactBinding.wrongAnswer
+            incorrectAnswer2 = newFactBinding.wrongAnswer2
+        }
+
+        val mode = intent.getStringExtra("EditMode")
+
+        if (mode == "true") {
+            question.setText(intent.getStringExtra("Question"))
+            answer.setText(intent.getStringExtra("CorrectAnswer"))
+            incorrectAnswer1.setText(intent.getStringExtra("IncorrectAnswer"))
+            incorrectAnswer2.setText(intent.getStringExtra("IncorrectAnswer2"))
         }
 
         cancel.setOnClickListener {
@@ -35,9 +48,42 @@ class NewFact : AppCompatActivity() {
                 val data = Intent()
                 data.putExtra("Question", question.text.toString())
                 data.putExtra("Answer", answer.text.toString())
+
+                val multiChoice = multiChoice()
+                when (multiChoice) {
+                    0 -> data.putExtra("Multi", "0")
+                    1 -> {
+                        data.putExtra("Multi", "1")
+                        data.putExtra("IncorrectAnswer1", incorrectAnswer1.text.toString())
+                    }
+                    2 -> {
+                        data.putExtra("Multi", "1")
+                        data.putExtra("IncorrectAnswer1", incorrectAnswer2.text.toString())
+                    }
+                    3 -> {
+                        data.putExtra("Multi", "3")
+                        data.putExtra("IncorrectAnswer1", incorrectAnswer1.text.toString())
+                        data.putExtra("IncorrectAnswer2", incorrectAnswer2.text.toString())
+                    }
+                }
+
                 setResult(RESULT_OK, data)
                 finish()
             }
+        }
+    }
+
+    fun multiChoice(): Int {
+        if ((incorrectAnswer1.text.toString().isNotBlank() && incorrectAnswer2.text.toString().isNotBlank())) {
+            return 3
+        } else if (incorrectAnswer1.text.toString().isNotBlank() || incorrectAnswer2.text.toString().isNotBlank()) {
+            if (incorrectAnswer1.text.toString().isNotBlank()) {
+                return 1
+            } else {
+                return 2
+            }
+        } else {
+            return 0
         }
     }
 }
